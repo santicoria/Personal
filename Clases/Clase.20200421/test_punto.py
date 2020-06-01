052639692af01bd3e1a7039cad7aadcde54c60f6
santicoria <s.coria@alumno.um.edu.ar>
2020-06-01 18:53:12 -0300

    clase20200421

--- Clases/Clase.20200421/test_punto.py
+++ Clases/Clase.20200421/test_punto.py
@@ -3,8 +3,8 @@ from punto import Punto, PuntoMejorado
 from parameterized import parameterized
 
 class TestPunto(unittest.TestCase):
-    @parameterized.expand([(4, 5, "(4, 5)")
-                            (-1, -2, "(-1, -2)")])
+    @parameterized.expand([(4, 5, "(4, 5)"),
+                           (-1, -2, "(-1, -2)")])
     def test_str_punto(self, x, y, string):
         punto = Punto(x, y)
         self.assertEqual(punto.__str__(), string)
